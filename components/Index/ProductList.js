3bc3f227c2d203ffb5cb805839b7841845ae90e9
unknown <ady.drozman2007@gmail.com>
2019-09-25 11:03:47 +0300

    add product list

--- components/Index/ProductList.js
+++ components/Index/ProductList.js
@@ -1,5 +1,18 @@
-function ProductList() {
-  return <>ProductList</>;
+import { Card } from 'semantic-ui-react';
+
+function ProductList({products}) {
+  function mapProductsToItems(products){
+    return products.map(product=>({
+      header:product.name,
+      image:product.mediaUrl,
+      meta:`$${product.price}`,
+      color:'teal',
+      fluid:true,
+      childKey:product._id,
+      href:`/product?_id=${product._id}`
+    }))
+  }
+  return <Card.Group stackable items={mapProductsToItems(products)} itemsPerRow="3" centered />
 }
 
 export default ProductList;
