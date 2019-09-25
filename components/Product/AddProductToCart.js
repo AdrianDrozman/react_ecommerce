01ab8e8777ae822dff4b060182640afd0254feea
AdrianD94 <ady.drozman2007@gmail.com>
2019-09-25 21:16:09 +0300

    add baseURL

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,7 +1,15 @@
 import { Input } from 'semantic-ui-react';
 
 function AddProductToCart() {
-  return <Input type="number" min="1" placeholder="Quantity" value={1} action={{color:'orange',content:"Add to Cart",icon:"plus cart"}} />
+  return (
+    <Input
+      type='number'
+      min='1'
+      placeholder='Quantity'
+      value={1}
+      action={{ color: 'orange', content: 'Add to Cart', icon: 'plus cart' }}
+    />
+  );
 }
 
 export default AddProductToCart;
