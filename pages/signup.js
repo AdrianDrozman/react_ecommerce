7fb771c91fbcaa4fc1281b2adb6f376a7001b403
adriand94 <ady.drozman2007@gmail.com>
2019-09-29 13:58:16 +0300

    add register functionallity

--- pages/signup.js
+++ pages/signup.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-
+import axios from 'axios';
 import { Button, Form, Icon, Message, Segment } from 'semantic-ui-react';
 import Link from 'next/link';
 import catchErrors from '../utils/catchErrors';
+import baseUrl from '../utils/baseUrl';
+import { handleLogin } from '../utils/auth';
 
 const INITIAL_USER = {
   name: '',
@@ -31,7 +33,10 @@ function Signup() {
     try {
       setLoading(true);
       setError('');
-      console.log(user);
+      const url = `${baseUrl}/api/signup`;
+      const payload = { ...user };
+      const response = await axios.post(url, payload);
+      handleLogin(response.data);
     } catch (error) {
       catchErrors(error, setError);
     } finally {
