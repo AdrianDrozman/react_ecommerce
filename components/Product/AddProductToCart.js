41a6372882e449c68fe47f0fbb33cbbb044df427
unknown <ady.drozman2007@gmail.com>
2019-09-30 14:37:20 +0300

    add product to cart function

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,13 +1,68 @@
-import { Input } from 'semantic-ui-react';
+import React, { useState,useEffect } from 'react'
 
-function AddProductToCart() {
+import { Input } from 'semantic-ui-react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import baseUrl from '../../utils/baseUrl'
+import  cookie  from 'js-cookie'
+import catchErrors from '../../utils/catchErrors';
+
+
+
+function AddProductToCart({user,productId}) {
+  const [quantity,setQuantity]=useState(1)
+  const [loading,setLoading] = useState(false)
+  const [success,setSuccess] = useState(false)
+  const router = useRouter();
+
+  useEffect(()=>{
+    let timeout;
+    if(success){
+      timeout=setTimeout(()=>setSuccess(false),2000)
+    }
+    return ()=>{
+      clearTimeout(timeout)
+    }
+  },[success])
+
+  const handleAddProduct = async() =>{
+    try{
+    setLoading(true)
+    const url = `${baseUrl}/api/cart`
+    const payload = {quantity,productId}
+    const token = cookie.get('token')
+    const headers={headers:{Authorization:token}}
+    await axios.put(url,payload,headers)
+    setSuccess(true)
+    }catch(error){
+      catchErrors(error,window.alert)
+     
+    }finally{
+      setLoading(false)
+    }
+
+}
+
   return (
     <Input
       type='number'
       min='1'
       placeholder='Quantity'
-      value={1}
-      action={{ color: 'orange', content: 'Add to Cart', icon: 'plus cart' }}
+      value={quantity}
+      onChange={e=>setQuantity(Number(e.target.value))}
+      action={
+        user && success ? {
+          color:'blue',
+          content:'Item Added!',
+          icon:'plus cart',
+          disabled:true
+        } :
+        user?{ color: 'orange', content: 'Add to Cart', icon: 'plus cart' ,loading,disabled:loading,onClick:handleAddProduct }:{
+        color:'blue',
+        content:'Sign Up To Purchase',
+        icon:"signup",
+        onClick:()=>router.push('/signup')
+      }}
     />
   );
 }
