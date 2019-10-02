3916199c0f66cbbb7f4c7ccc7ce26b8e8c267afb
unknown <ady.drozman2007@gmail.com>
2019-10-02 16:00:02 +0300

    add pagination for product list

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,23 @@ import React  from 'react';
 import axios from 'axios';
 import ProductList from '../components/Index/ProductList'
 import baseUrl from '../utils/baseUrl';
+import ProductPagination from '../components/Index/ProductPagination'
 
-function Home({products}) {
+function Home({products,totalPages}) {
   
-  return <ProductList products={products}/>;
+  return (<><ProductList products={products}/>
+          <ProductPagination totalPages={totalPages} /> </>);
 }
 
-Home.getInitialProps = async () => {
+Home.getInitialProps = async ctx => {
+  console.log(ctx.query)
+  const page = ctx.query.page ? ctx.query.page :"1"
+  const size =9;
+  const payload = {params:{page,size}}
   const url = `${baseUrl}/api/products`;
-  const response = await axios.get(url);
+  const response = await axios.get(url,payload);
   
-  return { products: response.data };
+  return  response.data;
 };
 
 export default Home;
